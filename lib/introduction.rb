2be3a581dc60313a2fd612db72c9b4d49f0e338b
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-06 15:43:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,5 @@
-def introduction_with_language(Manon, English)
-  puts "Hi, my name is #{Manon} and I am learning to program in #{English}."
+def introduction_with_language(name, language)
+  name = "Manon"
+  language = "English"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
